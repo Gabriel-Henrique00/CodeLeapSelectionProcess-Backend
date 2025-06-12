c2c5af2e30b76672992d5f201c11dcc564f8a5f1
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-06-12 16:38:01 -0300

    refactor(models): change Post.username to author ForeignKey

--- api/models.py
+++ api/models.py
@@ -1,13 +1,18 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class Post(models.Model):
-    username = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
     created_datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
 
     def __str__(self):
         return self.title
+
     class Meta:
         ordering = ['-created_datetime']
