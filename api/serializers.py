929ba283411e8325d59e892b3873418d4346be66
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-06-12 16:39:14 -0300

    feat(posts): associate author on creation and display username

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,8 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Post
-        fields = ['id', 'username', 'created_datetime', 'title', 'content']
-        read_only_fields = ['id', 'created_datetime']
+        fields = ['id', 'author_username', 'created_datetime', 'title', 'content']
--- api/views.py
+++ api/views.py
@@ -1,14 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Post
 from .serializers import PostSerializer
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    search_fields = ['username', 'title']
+    search_fields = ['author__username', 'title']
     filterset_fields = ['created_datetime']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
