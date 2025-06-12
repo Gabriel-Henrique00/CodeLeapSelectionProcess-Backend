0847851ef8103d5eeee9fa29e43e93361db364fc
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-06-12 16:12:35 -0300

    feat: add filtering and search to Posts

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['username', 'title']
+    filterset_fields = ['created_datetime']
