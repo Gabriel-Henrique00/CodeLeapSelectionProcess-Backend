6fc2d4349b7a40b0d7bf663ed8a1c12748d0ca6f
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-06-12 15:39:59 -0300

    feat: add default pagination to API

--- codeleap_project/settings.py
+++ codeleap_project/settings.py
@@ -134,4 +134,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 # DRF settings
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
